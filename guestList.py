fc1ed747e2506ce7884adc4a569510a96c8770ff
Andrew <jimboy911@gmail.com>
2018-05-01 14:44:48 -0400

    Update guestList.py

--- guestList.py
+++ guestList.py
@@ -1,11 +1,12 @@
 #guestList.py
 
-import csv
+import csv #import the csv module into the program
 
 def main():
 
-    with open("GuestList.txt", "r") as guestList:
-        guests = csv.reader(guestList)
+    with open("GuestList.txt", "r") as guestList: #opens GuestList.txt file, saves it as guestList, then closes the file
+        guests = csv.reader(guestList) #goes through the entire guestList and seperates each entry with a comma
+                                       #each line is its separate list entry
 
-        for eachGuest in guests:
-            print(eachGuest)
+        for eachGuest in guests: #iterates through each entry in the "guests" list
+            print(eachGuest) #prints out each entry in the "guests" list
